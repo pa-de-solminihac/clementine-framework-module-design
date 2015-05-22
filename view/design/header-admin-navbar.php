ea3c0fbce7075440488bfd090cbb84002518675a
BlouinBaptiste <baptiste.blouin@etu.univ-amu.fr>
2015-05-22 15:51:06 +0200

    Update header-admin-navbar.php

--- view/design/header-admin-navbar.php
+++ view/design/header-admin-navbar.php
@@ -1,4 +1,10 @@
-            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+<?php
+$is_iframe_class = 'not-an-iframe';
+if ($request->get('int', 'is_iframe')) {
+    $is_iframe_class = 'is-an-iframe';
+}
+?>            
+            <nav class="navbar navbar-default navbar-static-top <?php echo $is_iframe_class; ?>" role="navigation" style="margin-bottom: 0">
 <?php
 $this->getBlock('design/header-admin-navbar-header', $data, $request);
 ?>
@@ -7,7 +13,7 @@ $this->getBlock('design/header-admin-navbar-header', $data, $request);
 <?php
 if (!empty($data['navbar-sidebar'])) {
 ?>
-                <nav class="navbar-default sidebar" role="navigation">
+                <nav class="navbar-default sidebar <?php echo $is_iframe_class; ?>" role="navigation">
                     <div class="sidebar-nav navbar-collapse">
                         <ul class="nav" id="side-menu">
 <?php
